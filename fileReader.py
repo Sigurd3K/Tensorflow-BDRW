29be79d8374e2b77ad0ebc5fdc08e86afae9cca2
Sigurd Van Leuven <sigurdvanleuven@gmail.com>
2017-02-24 15:38:13 +0100

    Filereader
    Nog niet functioneel maar alles werkt tot nu toe.

--- fileReader.py
+++ fileReader.py
@@ -11,16 +11,40 @@ Preprocessing (optional)
 Example queue
 """
 
+import tensorflow as tf
+
 print(" ")
 print("== fileReader.py ==")
 
-FILEDIR = 'data/BDRW_train'
-TRAINING_DIR= FILEDIR + '\BDRW_train_1'
-VALIDATION_DIR= FILEDIR + '\BDRW_train_2'
+FILEDIR = './data/BDRW_train'
+TRAINING_DIR= FILEDIR + '/BDRW_train_1/'
+VALIDATION_DIR = FILEDIR + '/BDRW_train_2/'
 LABEL_FILE = 'filedir'
+EPOCH_LIMIT = 50
+FILES_VALIDATION = 0
 
 def filenameLister():
+	FILES_TRAINING = tf.train.string_input_producer(
+	tf.train.match_filenames_once(TRAINING_DIR + "digit_*.jpg"))
 	print("Filedir: %s" % (FILEDIR))
+	#print("Training files dir: %s" % (TRAINING_DIR))
+	#print(type(FILES_TlRAINING))
+	return FILES_TRAINING
+
+FILES_TRAINING = filenameLister()
+
+# print("-------")
+# print(tf.gfile.Glob(TRAINING_DIR + "digit_*.jpg"))
+
+# print(FILES_TRAINING)
+
+print(type(FILES_TRAINING))
 
 
-filenameLister()
+image_reader = tf.WholeFileReader()
+
+# print(FILES_TRAINING)
+
+_, image_file = image_reader.read(FILES_TRAINING)
+
+image = tf.image.decode_jpeg(image_file)
