05fb296a06cb2d8cbb1661380ed414e20d533cef
Sigurd Van Leuven <sigurdvanleuven@gmail.com>
2017-02-28 17:10:57 +0100

    Correct label file dir

--- fileReader.py
+++ fileReader.py
@@ -21,13 +21,14 @@ print("== fileReader.py ==")
 FILEDIR = './data/BDRW_train'
 TRAINING_DIR= FILEDIR + '/BDRW_train_1/'
 VALIDATION_DIR = FILEDIR + '/BDRW_train_2/'
-LABEL_FILE = FILEDIR+ '/BDRW_train_2/labels.csv'
+LABEL_FILE = FILEDIR + '/BDRW_train_2/labels.csv'
 EPOCH_LIMIT = 50
 FILES_VALIDATION = 0
 BATCH_SIZE = 50
 NUM_PREPROCESS_THREADS = 1
 MIN_QUEUE_EXAMPLES= 256
 
+print(LABEL_FILE)
 
 W = tf.Variable(tf.zeros([2304,10]))
 b = tf.Variable(tf.zeros([10]))
