cc6892ae1d92196b38e895792f5f8240d47ffec3
Sigurd Van Leuven <sigurdvanleuven@gmail.com>
2017-03-07 15:42:15 +0100

    Necessary Vars

--- imageClassifier.py
+++ imageClassifier.py
@@ -1,6 +1,37 @@
 import tensorflow as tf
+import math
 from colored import fg, bg, attr
 import fileReader as fR
+
+# -- Classifier variables --
+
+layer_sizes = [2304, 3000, 500, 250, 250, 250, 10]
+shapes = zip(layer_sizes[:-1], layer_sizes[1:]) #Element 1 en 2 worden samen in tuple gestoken enz omdat lists ten opzichte van elkaar met 1 verschuiven.
+number_of_layers = len(layer_sizes) - 1
+
+starter_learning_rate = 0.02
+
+batch_size = 50
+
+inputs = tf.placeholder(tf.float32, shape=(None, layer_sizes[0]))
+outputs = tf.placeholder(tf.float32)
+
+
+def bi(inits, size, name):
+	return tf.Variable(inits * tf.ones([size]), name=name)
+
+
+def wi(shape, name):
+	return tf.Variable(tf.random_normal(shape, name=name)) / math.sqrt(shape[0])
+
+weights = {
+	'W': [wi(s, "W") for s in shapes],
+	'V': [wi(s[::-1], "V") for s in shapes],
+	'beta': [bi(0.0, layer_sizes[l+1], "beta") for l in range(number_of_layers)],
+	'gamma': [bi(0.0, layer_sizes[l+1], "beta") for l in range(number_of_layers)],
+	}
+
+# -- End of Classifier variables --
 
 with tf.Session() as sess:
 	tf.global_variables_initializer().run()
