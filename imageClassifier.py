69477fdb209924cb6fa28bbda049a0a9d2fbc5ce
Sigurd Van Leuven <sigurdvanleuven@gmail.com>
2017-03-13 09:19:15 +0100

    Pyplot check image

--- imageClassifier.py
+++ imageClassifier.py
@@ -2,6 +2,8 @@ import tensorflow as tf
 import math
 from colored import fg, bg, attr
 import fileReader as fR
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
 # -- Classifier variables --
 
@@ -54,19 +56,43 @@ with tf.Session() as sess:
 	# 	batch_xs, batch_ys = sess.run([images])
 	# 	sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
+	printedTest = False
+
 	try:
 		while not coord.should_stop():
 			looper += 1
 			print(looper)
 			# print("WHILE TRUE")
 			image_name_batch_b, image_class_batch_b = sess.run([fR.image_tra_name_batch, fR.image_tra_class_batch]) # EERSTE VARS NIET HETZELFDE NOEMEN ALS DIE IN RUN
-			image_class_batch_c = sess.run([fR.images])
+			image_batch_c = sess.run([fR.images])
 			# print(type(image_name_batch))
-			print(" ")
-			print("======== Batches uit de CSV ========")
-			print(" ")
-			print(image_name_batch_b, image_class_batch_b)
-			print(image_class_batch_c)
+			# print(" ")
+			# print("======== Batches uit de CSV ========")
+			# print(" ")
+			# print(image_name_batch_b, image_class_batch_b)
+			# print(image_class_batch_c)
+
+			if printedTest == False:
+				plt.style.use("ggplot")
+				print('%s%s =========== TEST SAMPLE =========== %s' % (fg('white'), bg('green'), attr('reset')))
+				print(image_name_batch_b)
+				print(" ")
+				print(image_class_batch_b)
+				imt = plt.imshow(image_batch_c[0][0]/255)
+
+				print(img)
+				print(" Afbeelding uit mijn batch: ")
+				print(image_batch_c[0][0]/255)
+
+				# print(image_batch_c[0][0].shape)
+				# print(img.shape)
+				# raw_input()
+				input("Press Enter to continue...")
+				# print(img)
+
+				print('%s%s ======= END OF TEST SAMPLE ======= %s' % (fg('white'), bg('green'), attr('reset')))
+				printedTest = True
+
 	except tf.errors.OutOfRangeError:
 		# Bij epoch = 1 in de queue geeft TextLineReader of de queue een outOfRange exception als er geen lines meer over zijn om te readen
 		print(looper)
