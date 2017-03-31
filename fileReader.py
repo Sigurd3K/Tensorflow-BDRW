91ee5b21792409396681ca566f171d4c2007021f
Sigurd Van Leuven <sigurdvanleuven@gmail.com>
2017-03-31 14:26:09 +0200

    Initial values change

--- fileReader.py
+++ fileReader.py
@@ -23,12 +23,12 @@ print(LABEL_FILE)
 
 
 def weight_variable(shape):
-  initial = tf.truncated_normal(shape, stddev=0.1)
+  initial = tf.truncated_normal(shape, stddev=0.5)
   return tf.Variable(initial, name="Weight")
 
 
 def bias_variable(shape):
-  initial = tf.constant(0.1, shape=shape)
+  initial = tf.constant(0.5, shape=shape)
   return tf.Variable(initial, name="Biases")
 
 
