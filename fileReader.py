ef06591475426df9d2c62e06d2aa68e6b319b438
Sigurd Van Leuven <sigurdvanleuven@gmail.com>
2017-04-06 11:31:57 +0200

    Bugfix

--- fileReader.py
+++ fileReader.py
@@ -151,11 +151,14 @@ b_conv1 = bias_variable([32])
 h_conv1 = tf.nn.relu(conv2d(image_drop1, W_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1) # 24x24
 
+keep_prob2 = tf.placeholder(tf.float32)
+h_fc1_drop1 = tf.nn.dropout(h_pool1, keep_prob2)
+
 # Second Convolutional Layer:
 W_conv2 = weight_variable([8, 8, 32, 64])
 b_conv2 = bias_variable([64])
 
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+h_conv2 = tf.nn.relu(conv2d(h_fc1_drop1, W_conv2) + b_conv2)
 h_pool2 = max_pool_2x2(h_conv2) #12x12
 
 # Densely Connected Layer:
@@ -166,7 +169,6 @@ h_pool2_flat = tf.reshape(h_pool2, [-1, 12*12*64])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
 # Dropout Layer:
-keep_prob2 = tf.placeholder(tf.float32)
 h_fc1_drop2 = tf.nn.dropout(h_fc1, keep_prob2)
 
 # Readout Layer:
