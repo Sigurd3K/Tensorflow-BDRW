1bf748de2c576c1eff9972fadb7e368e8a292719
Sigurd Van Leuven <sigurdvanleuven@gmail.com>
2017-04-07 17:51:22 +0200

    Keras Trainers

--- trainer_B.py
+++ trainer_B.py
@@ -1,13 +1,28 @@
+"""Full Keras code with Tensorflow FileReader backend, does not work yet"""
+
+
+import fileReader as fR
+
+import keras
 from keras.models import Sequential
 from keras.layers import Dense, Activation
 
 model = Sequential()
 
-model.add(Dense(units=64, input_dim=100))
+x_train, y_train, _, _ = fR.return_training_set()
+x_test, y_test, _, _ = fR.return_eval_set()
+
+# y_train = keras.utils.to_categorical(y_train, 10)
+
+model.add(Dense(50, activation='relu', input_dim=6912))
 model.add(Activation('relu'))
-model.add(Dense(units=10))
+# model.add(Dense(units=10))
 model.add(Activation('softmax'))
 
-model.compile(loss='categorical_crossentropy',
-              optimizer="sgd",
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
               metrics=['accuracy'])
+
+
+
+model.fit(x_train, y_train, epochs=10, batch_size=50)
