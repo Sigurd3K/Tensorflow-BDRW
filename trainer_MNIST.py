aee817023168c15493d3bb2dac51a3ea3cfe5bb7
Sigurd3K <sigurdvanleuven@gmail.com>
2017-04-13 14:47:24 +0200

    Start of MNIST Trainer

---
+++ trainer_MNIST.py
@@ -0,0 +1,33 @@
+"""
+Full Tensorflow code with keras to manage layers
+https://blog.keras.io/keras-as-a-simplified-interface-to-tensorflow-tutorial.html
+"""
+
+import tensorflow as tf
+import math
+from colored import fg, bg, attr
+import fileReader as fR
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+with sess.as_default():
+
+	tf.global_variables_initializer().run()
+	tf.local_variables_initializer().run()
+
+	writer = tf.summary.FileWriter("./logs")
+	writer.add_graph(sess.graph)
+
+	coord = tf.train.Coordinator()
+	threads = tf.train.start_queue_runners(coord=coord)
+
+	looper = 0
+
+	printedTest = False
+
+	for x in range(loopAmount):
+	input("Press Enter to continue...")
+
+
+	coord.request_stop()
+	coord.join(threads)
